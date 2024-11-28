6a90ec7d6a2f8bf258c52dfbccdec291f87b823e
thesubtleartist <thesubtleartist@yahoo.com>
2024-11-27 21:39:46 -0600

    add and updated

--- Lists_Basic.py
+++ Lists_Basic.py
@@ -2,42 +2,46 @@
 
 
 nums =[25, 30, 15, 12, 15, 56, 95, 69] # List requires the brackets
-print(nums)
-print(nums[2])
-print(nums[4:])
-print(nums[-2])
-print(nums[:6])
-print(nums[:-4])
+print("Entire List: ",nums)
+print("Index 2: ", nums[2])
+print("Index 4: ", nums[4:])
+print("Index -2: ", nums[-2])
+print("Slide ':6': ",nums[:6])
+print("Slice ':-4': ", nums[:-4])
 
 names = ['Jo', 'Bob', 'Square', 'Pants', 'Keen']
-print(names[4])
+print("Index 4: ",names[4])
 
 mixed = ['slum', 'dog', 4, 4.5, -10]
-print(mixed)
+print("Mixed List: ",mixed)
 
 print("Append a value")
-m = int(input("Provide a number"))
-nums.append(m)
+# m = int(input("Provide a number: "))
+nums.append(int(input("Provide a number: ")))
 print("nums is now " + str(nums))
 
 print("Remove an element")
 print(nums)
-n = int(input("Choose one of these numbers"))
+n = int(input("Choose one of these numbers: "))
 nums.remove(n)
 print("nums no longer contains " + str(n))
 print(nums)
 
 print("Deleting Multiple Values")
 del nums[3:]
-
-print("Adding Multiple Values")
-nums.extend([20, 25, 30, 35, 40, 45, 98, 42, 452,])
 print(nums)
+
+print("Exending the List")
+nums.extend([20, 25, 30, 35, 40, 45, 98, 42, 452,])
+print("Extended: ", nums)
+
+srtNums=nums.sort()
+print("Sort: ", srtNums)
+revNums=nums.sort(reverse=True)
+print("Reverse: ", revNums)
+
 
 #Mathematics
-min(nums)
-max(nums)
-sum(nums)
-print(nums)
-nums.sort()
-print(nums)
+print("Min method: ",min(nums))
+print("Max method: ", max(nums))
+print("sum method: ", sum(nums))
---
+++ Lists_Linked.py
@@ -0,0 +1,2 @@
+from IPython.display import Image
+Image(filename='resources/linked_list.png')
