500364ef3bda778fee6dddcbaf44156101cf2d00
kali <kali@mysterious.internal>
2025-11-12 16:52:03 -0600

    begin corrections

--- API_Sensitive.py
+++
@@ -1,2 +0,0 @@
-def getApiKey():
-	return '7b0181303d49543e6fe6b50be426cd5b'
--- Socket_UDP_Server.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(("0.0.0.0", 1337))
-
-while True:
-        data, addr = server_socket.recvfrom(1024)
-        print(data)
